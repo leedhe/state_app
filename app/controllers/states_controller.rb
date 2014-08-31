89131fb40c1cac8296c3c7952799cbeb27183485
Your Name <you@example.com>
2014-08-31 15:17:14 +0000

    first or second commit

---
+++ app/controllers/states_controller.rb
@@ -0,0 +1,56 @@
+class StatesController < ApplicationController
+
+  def search_states
+  end
+  
+  def index
+    if params['search']
+      @states = State.where(abbreviation: params['search'].upcase)
+    else
+      @states = State.all
+    end
+  end
+
+  def show
+    @state = State.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @state = State.new
+    @state.name = params[:name]
+    @state.abbreviation = params[:abbreviation]
+
+    if @state.save
+      redirect_to "/states"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @state = State.find_by(id: params[:id])
+  end
+
+  def update
+    @state = State.find_by(id: params[:id])
+    @state.name = params[:name]
+    @state.abbreviation = params[:abbreviation]
+
+    if @state.save
+      redirect_to "/states/#{ @state.id }"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @state = State.find_by(id: params[:id])
+    @state.destroy
+
+
+    redirect_to "/states"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  # Routes for the State resource:
+  # CREATE
+  get '/new_state' => 'states#new'
+  get '/create_state' => 'states#create'
+
+  # READ
+  get '/states' => 'states#index'
+  get '/states/:id' => 'states#show'
+
+  # UPDATE
+  get '/states/:id/edit' => 'states#edit'
+  get '/states/:id/update' => 'states#update'
+
+  # DELETE
+  get '/states/:id/destroy' => 'states#destroy'
+  
+  # MISC
+  get '/search_states' => 'states#search_states'
+  #------------------------------
+
+  get '/home' => 'application#home'
+end
