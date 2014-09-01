e22b927cff54bfbfc4561978792b2172ec45c13b
Your Name <you@example.com>
2014-09-01 11:42:42 +0000

    minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   # Routes for the State resource:
   # CREATE
-  get '/new_state' => 'states#new'
-  get '/create_state' => 'states#create'
+ # get '/new_state' => 'states#new'
+ # get '/create_state' => 'states#create'
 
   # READ
-  get '/states' => 'states#index'
-  get '/states/:id' => 'states#show'
+   get '/states' => 'states#index'
+ # get '/states/:id' => 'states#show'
 
   # UPDATE
-  get '/states/:id/edit' => 'states#edit'
-  get '/states/:id/update' => 'states#update'
+ # get '/states/:id/edit' => 'states#edit'
+ # get '/states/:id/update' => 'states#update'
 
   # DELETE
-  get '/states/:id/destroy' => 'states#destroy'
+ # get '/states/:id/destroy' => 'states#destroy'
   
   # MISC
   get '/search_states' => 'states#search_states'
